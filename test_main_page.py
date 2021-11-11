1a0f5e9c07e25e7ae77524e32c28486059b38228
evgsaf <evgenii.safarov@steor.tech>
2021-11-11 21:35:16 +0300

    4.2.8 red

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
 from pages.main_page import MainPage
-
+from pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -12,3 +12,21 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_login_page_login_form_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+def test_login_page_reg_form_present(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+def test_login_page_login_substring_present_in_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()

496444306f9dbfe9a93b2384c3df8b5d7fd872cd
evgsaf <evgenii.safarov@steor.tech>
2021-11-11 21:35:41 +0300

    4.2.8 green

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert not "login" in self.browser.current_url, "No 'login' substring in current url"
+        assert "login" in self.browser.current_url, "No 'login' substring in current url"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert not self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert not self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Reg form is not presented"
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Reg form is not presented"
